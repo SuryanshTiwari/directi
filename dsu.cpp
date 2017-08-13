a513fbc60c15740884b6b5f29ecd6e69af8592bd
suryansh tiwari <f2013777@pilani.bits-pilani.ac.in>
2017-08-13 22:10:41 +0530

    dsu

--- dsu.cpp
+++ dsu.cpp
@@ -1,88 +1,54 @@
-include "bits/stdc++.h"
+
+
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-ll root (ll Arr[ ] ,ll i)
+
+
+int sz[1000005],parent[1000004],arr[1000005] ;
+int N , M ;
+int root(int a)
 {
-    while(Arr[ i ] != i)
-    {
-        Arr[ i ] = Arr[ Arr[ i ] ] ; 
-i = Arr[ i ]; 
+    while(a!=parent[a]) {
+        a = parent[a] ;
     }
-return i;
+    return a ;
 }
-void weightedunion(ll Arr[ ],ll size[ ],ll A,ll B)
+void join(int a,int b)
 {
-    int root_A = root(Arr,A);
-    int root_B = root(Arr,B);
-    if(root_A == root_B)return;
-    if(size[root_A] < size[root_B ])
-    {
-Arr[ root_A ] = Arr[root_B];
-size[root_B] += size[root_A];
+    a = root(a) , b = root(b) ;
+    if(sz[a] > sz[b]) {
+        parent[b] = a ;
+        sz[a] += sz[b] ;
+    }
+    else {
+        parent[a] = b ;
+        sz[b] += sz[a] ;
+    }
+    return ;
 }
-    else
-    {
-Arr[ root_B ] = Arr[root_A];
-size[root_A] += size[root_B];
-}
- 
-}
-void initialize( ll Arr[ ], ll size[], ll N)
-{
-    for(int i = 1;i<=N;i++)
-    {
-Arr[ i ] = i ;
-size[ i ] = 1;
-}
-}
+vector<int> haha[1000005] ;
 int main()
 {
-ll n, m, x;
-cin >> n >> m >> x;
-int a[n+1];
-ll arr[n+1];
-ll size[n+1];
-// memset(a, 0, sizeof a);
-ll kk[n+1];
-// memset(kk,0, sizeof kk);
-ll visited[n+1];
-// memset(visited, 0, sizeof visited);
-for(int i = 0; i <= n; i++)visited[i] = kk[i] = a[i] = 0;
-for(int i = 0; i < m; i++)
-{
-ll xxx;
-cin >> xxx;
-a[xxx] = 1;
-}
- 
-initialize(arr, size, n);
- 
-for(int i = 0; i < x; i++)
-{
-ll g, h;
-cin >> g >> h;
-weightedunion(arr, size, g, h);
-}
-memset(visited, 0, sizeof visited);
-long long sum = 0;
-for(int i = 1; i <= n; i++)
-{
-    if(a[i] == 1)
-    {
-       ll roott = root(arr, i);
-       kk[roott] += 1;
+    scanf("%d%d",&N,&M) ;
+    rep(i,1,N) {
+        scanf("%d",&arr[i]) ;
+        sz[i] = 1 ;
+        parent[i] = i ;
     }
-}
-for(int i = 1; i <= n; i++)
-{
-    ll roott = root(arr, i);
-    if(visited[roott] == 0)
-    {
-  //      cout << kk[roott] << " " ;
-        visited[roott] = 1;
-        sum += size[roott]*kk[roott];
+    rep(i,1,M) {
+        int u , v ;
+        scanf("%d%d",&u,&v) ;
+        join(u,v) ;
     }
-}
-cout << sum << "\n";
-return 0;
+    rep(i,1,N) {
+        haha[root(i)].push_back(arr[i]) ;
+    }
+    rep(i,1,N) {
+        sort(all(haha[i])) ;
+    }
+    rep(i,1,N) {
+        printf("%d ",haha[root(i)].back()) ;
+        haha[root(i)].pop_back() ;
+    }
+    return 0;
 }
